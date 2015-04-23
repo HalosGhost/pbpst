a50ab4126c1674952af07ec442d32925a1d3aaf9
halosghost <halosghost@archlinux.info>
2015-04-22 21:28:22 -0500

    Simplify enum and realign

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "main.h"
 
 struct ptpst_state {
-    enum { NONE, SYNC, REMOVE, UPDATE/*, DATABASE */ } cmd;
+    enum { NON, SNC, RMV, UPD/*, DBS */ } cmd;
     bool help;
 };
 
@@ -19,22 +19,22 @@ main (signed argc, char * argv []) {
         return EXIT_FAILURE;
     }
 
-    struct ptpst_state state = { .cmd = NONE, .help = false };
+    struct ptpst_state state = { .cmd = NON, .help = false };
     for ( signed oi = 0, c = getopt_long(argc, argv, "SRUP:hvsflLpru", os, &oi);
           c != -1; c = getopt_long(argc, argv, "SRUP:hvsflLpru", os, &oi) ) {
 
         switch ( c ) {
             case 'S':
-                if ( state.cmd != NONE ) { goto fail_multiple_cmds; }
-                state.cmd = SYNC; break;
+                if ( state.cmd != NON ) { goto fail_multiple_cmds; }
+                state.cmd = SNC; break;
 
             case 'R':
-                if ( state.cmd != NONE ) { goto fail_multiple_cmds; }
-                state.cmd = REMOVE; break;
+                if ( state.cmd != NON ) { goto fail_multiple_cmds; }
+                state.cmd = RMV; break;
 
             case 'U':
-                if ( state.cmd != NONE ) { goto fail_multiple_cmds; }
-                state.cmd = UPDATE; break;
+                if ( state.cmd != NON ) { goto fail_multiple_cmds; }
+                state.cmd = UPD; break;
 
             case 257: printf(version_str); return EXIT_SUCCESS;
 
@@ -48,11 +48,10 @@ main (signed argc, char * argv []) {
 
     if ( state.help ) {
         switch ( state.cmd ) {
-            case SYNC:   printf("%s%s",   sync_help, gen_help); break;
-            case REMOVE: printf("%s%s",   rem_help,  gen_help); break;
-            case UPDATE: printf("%s%s",   upd_help,  gen_help); break;
-            case NONE:   printf("%s%s%s", cmds_help, gen_help,
-                                more_info);                     break;
+            case SNC: printf("%s%s",   sync_help, gen_help);            break;
+            case RMV: printf("%s%s",   rem_help,  gen_help);            break;
+            case UPD: printf("%s%s",   upd_help,  gen_help);            break;
+            case NON: printf("%s%s%s", cmds_help, gen_help, more_info); break;
         } return EXIT_SUCCESS;
     }
 
