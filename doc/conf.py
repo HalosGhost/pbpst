4f0e17da61e645e89c8739852f97a9bfb8bc889d
HalosGhost <halosghost@archlinux.info>
2015-07-08 17:27:36 -0500

    Build manpage from source

---
+++ doc/conf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+project = 'ptpst'
+master_doc = 'ptpst'
+source_suffix = '.rst'
+
+show_authors = False
+today_fmt = '%d %b %Y'
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('ptpst', 'ptpst', 'A simple libcurl/C pastebin utility for pb deployments', [], 1),
+]
