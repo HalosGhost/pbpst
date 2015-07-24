5f5e3a73093309f30eb08e2c2065bc6616cfa8fc
halosghost <halosghost@archlinux.info>
2015-07-23 19:19:18 -0500

    Dramatic cleanup (readability, style and compactness)

--- src/callback.c
+++ src/callback.c
@@ -1,62 +1,44 @@
-#include <stdio.h>
-#include <curl/curl.h>
-
+#include <stdint.h>    // uint8_t
+#include <stdio.h>     // fputs(), putchar(), printf(), fflush()
+#include <inttypes.h>  // PRIu8
+#include <curl/curl.h> // curl_off_t
 #include "callback.h"
 
 /* adapted from pacman */
-static
-void fill_progress (const int bar_percent, const int disp_percent,
-                    const int proglen)
-{
+static void
+fill_progress (const uint8_t bar_percent, const uint8_t proglen) {
+
     /* 8 = 1 space + 1 [ + 1 ] + 5 for percent */
-    const int hashlen = proglen > 8 ? proglen - 8 : 0;
-    const int hash = bar_percent * hashlen / 100;
-    static int lasthash = 0, mouth = 0;
-    int i;
+    const uint8_t hashlen = proglen > 8 ? proglen - 8 : 0,
+                  hash    = bar_percent * hashlen / 100;
 
-    if(bar_percent == 0) {
-        lasthash = 0;
-        mouth = 0;
-    }
-
-    if(hashlen > 0) {
+    if ( hashlen > 0 ) {
         fputs(" [", stdout);
-        for (i = hashlen; i > 0; --i) {
-            if (i > hashlen - hash)
-                putchar('#');
-            else
-                putchar('-');
-        }
-        putchar(']');
+        for ( uint8_t i = hashlen; i; --i ) {
+            putchar(i > (hashlen - hash) ? '#' : '-');
+        } putchar(']');
     }
 
     /* print display percent after progress bar */
-    if(proglen >= 5)
-        printf(" %3d%%", disp_percent);
+    if ( proglen >= 5 ) { printf(" %3" PRIu8 "%%", bar_percent); }
 
-    if(bar_percent == 100)
-        putchar('\n');
-    else
-        putchar('\r');
-
+    putchar(bar_percent == 100 ? '\n' : '\r');
     fflush(stdout);
 }
 
-int
-pb_progress_cb (void *client,
+signed
+pb_progress_cb (void * client,
                 curl_off_t dltotal, curl_off_t dlnow,
                 curl_off_t ultotal, curl_off_t ulnow) {
 
-    long progress;
-    static long last_progress;
+    static curl_off_t last_progress;
+    curl_off_t progress = ultotal ? ulnow * 100 / ultotal : 0;
 
-    progress = ultotal ? ulnow * 100 / ultotal : 0;
+    if ( progress == last_progress ) { return 0; }
 
-    if (progress == last_progress)
-        return 0;
-
-    fill_progress((int)progress, (int)progress, 40);
+    fill_progress((uint8_t )progress, 40);
     last_progress = progress;
-
     return 0;
 }
+
+// vim: set ts=4 sw=4 et:
