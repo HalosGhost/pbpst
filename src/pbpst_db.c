8ec29eae983d5f3cafd93dad329b2b24dd078846
halosghost <halosghost@archlinux.info>
2015-07-24 21:57:44 -0500

    simple cleanups

--- src/pbpst_db.c
+++ src/pbpst_db.c
@@ -1,22 +1,26 @@
-#include <stdio.h>    // fprintf(), FILE
+#include <stdio.h>    // fputs(), remove()
 #include <fcntl.h>    // open()
-#include <unistd.h>   // chdir()
-#include <stdlib.h>   // exit()
+#include <unistd.h>   // close()
+#include <sys/stat.h> // S_IRUSR
+#include <stdlib.h>   // EXIT_FAILURE, EXIT_SUCCESS
 #include "pbpst_db.h"
 
 signed
 db_lockfile_init (void) {
-    signed fd; // Integer for file descriptor
-    if ( (fd = open("/tmp/pbpst.lck", O_RDONLY | O_CREAT | O_EXCL, S_IRUSR)) == -1 ) {
-        fprintf(stderr, "Cannot open /tmp/ptpst.lck.\n");
+
+    signed fd, oflag = O_RDONLY | O_CREAT | O_EXCL;
+    if ( (fd = open("/tmp/pbpst.lck", oflag, S_IRUSR)) == -1 ) {
+        fputs("Cannot open /tmp/ptpst.lck.\n", stderr);
         return EXIT_FAILURE;
     }
+
     close(fd);
     return EXIT_SUCCESS;
 }
 
 signed
 db_lockfile_cleanup (void) {
+
     remove("/tmp/ptpst.lck");
     return EXIT_SUCCESS;
 }
