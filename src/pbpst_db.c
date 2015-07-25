1013fa143e725e652c1ee9c63f710302d7cea688
Dolores Portalatin <meskarune@gmail.com>
2015-07-24 21:19:51 -0400

    added header for open and 2 lines to open/remove file

--- src/pbpst_db.c
+++ src/pbpst_db.c
@@ -1,17 +1,18 @@
 #include <stdio.h>    // fprintf(), FILE
+#include <fcntl.h>    // open()
 #include <unistd.h>   // chdir()
 #include <stdlib.h>   // exit()
 #include "pbpst_db.h"
 
 signed
 db_lockfile_init (void) {
-
+    open("/tmp/ptpst.lock", O_RDWR | O_CREAT, 0200);
     return 0;
 }
 
 signed
 db_lockfile_cleanup (void) {
-
+    remove("/tmp/ptpst.lock");
     return 0;
 }
 
